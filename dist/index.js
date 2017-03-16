fd4dbcea0c9926f7f18a8dd35e6c659783bedc56
Juan Crisóstomo <juan.crisostomo@sngular.team>
2017-03-16 14:27:32 -0600

    se modifica para agregar más elementos

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _Drawer = require("./src/Drawer");
+
+var _Drawer2 = _interopRequireDefault(_Drawer);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
---
+++ src/index.es6
@@ -0,0 +1,1 @@
+import Drawer from "./src/Drawer";
